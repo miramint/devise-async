fd0bc8d30760806002391963eeaf676673e17d6c
Fabian Stehle <fabi@fstehle.de>
2014-07-08 17:37:19 +0200

    Use dead-letter-exchange for devise_mailer queue

--- lib/devise/async/backend/sneakers.rb
+++ lib/devise/async/backend/sneakers.rb
@@ -5,7 +5,10 @@ module Devise
     module Backend
       class Sneakers < Base
         include ::Sneakers::Worker
-        from_queue "devise_mailer"
+        from_queue "devise_mailer",
+                   :arguments => {
+                       :'x-dead-letter-exchange' => 'devise_mailer-retry'
+                   }
 
         def self.enqueue(*args)
           ::Sneakers::Publisher.new.publish(args.to_json, {:to_queue => self.queue_name})
